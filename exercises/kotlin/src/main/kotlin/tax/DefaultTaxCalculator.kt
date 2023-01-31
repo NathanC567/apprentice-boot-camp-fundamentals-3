0d56299f3de0c0ee3e2570f01f28dd023d874efe
Nathan Cullen <Nathan.Cullen@autotrader.co.uk>
2023-01-31 13:56:51 +0000

    Added calculator for Diesel & Alternative Fuel

--- exercises/kotlin/src/main/kotlin/tax/DefaultTaxCalculator.kt
+++ exercises/kotlin/src/main/kotlin/tax/DefaultTaxCalculator.kt
@@ -1,25 +1,59 @@
 package tax
 
-import tax.TaxCalculator
-import tax.Vehicle
-
 class DefaultTaxCalculator : TaxCalculator() {
     override fun calculateTax(vehicle: Vehicle?): Int {
-        return when (vehicle?.co2Emissions) {
-            0 -> 0
-            in 1..50 -> 10
-            in 1..75 -> 25
-            in 1..90 -> 105
-            in 1..100 -> 125
-            in 1..110 -> 145
-            in 1..130 -> 165
-            in 1..150 -> 205
-            in 1..170 -> 515
-            in 1..190 -> 830
-            in 1..225 -> 1240
-            in 1..255 -> 1760
-            else -> 2070
+        return when (vehicle?.fuelType) {
+            FuelType.PETROL -> {
+                when (vehicle.co2Emissions) {
+                    0 -> 0
+                    in 1..50 -> 10
+                    in 51..75 -> 25
+                    in 76..90 -> 105
+                    in 91..100 -> 125
+                    in 101..110 -> 145
+                    in 111..130 -> 165
+                    in 131..150 -> 205
+                    in 151..170 -> 515
+                    in 171..190 -> 830
+                    in 191..225 -> 1240
+                    in 226..255 -> 1760
+                    else -> 2070
+                }
+            }
+            FuelType.ALTERNATIVE_FUEL -> {
+                when (vehicle.co2Emissions) {
+                    0 -> 0
+                    in 1..50 -> 0
+                    in 51..75 -> 15
+                    in 76..90 -> 95
+                    in 91..100 -> 115
+                    in 101..110 -> 135
+                    in 111..130 -> 155
+                    in 131..150 -> 195
+                    in 151..170 -> 505
+                    in 171..190 -> 820
+                    in 191..225 -> 1230
+                    in 226..255 -> 1750
+                    else -> 2060
+                }
+            }
+            FuelType.DIESEL -> {
+                when (vehicle.co2Emissions) {
+                    0 -> 0
+                    in 1..50 -> 25
+                    in 51..75 -> 105
+                    in 76..90 -> 125
+                    in 91..100 -> 145
+                    in 101..110 -> 165
+                    in 111..130 -> 205
+                    in 131..150 -> 515
+                    in 151..170 -> 830
+                    in 171..190 -> 1240
+                    in 191..225 -> 1760
+                    in 226..255 -> 2070
+                    else -> 2070
+                }
+            } else -> 0
         }
     }
-
 }
